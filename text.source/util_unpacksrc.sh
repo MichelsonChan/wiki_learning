eb409009c1902e095504af166fc81af4af0b89d3
MichelsonChan <michelsonchan.apple.hksar@gmail.com>
2014-10-27 20:51:16 +0800

    add file for unpack

---
+++ text.source/util_unpacksrc.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+mkdir ENWIKI-20140903-PAGES-META-CURRENT01
+/bin/sh ./LIBRARY/util_xmldump2files_MichaelModify.py ../dump.source/enwiki-20140903-pages-meta-current1.xml-p000000010p000010000 ./ENWIKI-20140903-PAGES-META-CURRENT01/
